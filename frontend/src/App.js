3ba22c53af71707b9193924a9a23940bb47b1bd9
David A. Vargas <davidavargas.dev@gmail.com>
2024-08-29 18:24:51 -0500

    Navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar/Navbar';
 import CheckInForm from './CheckIn/CheckInForm';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <h1>Welcome to Hope Auto Check-In</h1>
         {/* Add your app content here */}
