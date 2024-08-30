8be013c64807e90f74325b4d7700da02f00e07cc
David A. Vargas <davidavargas.dev@gmail.com>
2024-08-30 12:01:25 -0500

    admin login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import AdminLogin from './AdminLogin/AdminLogin';
 import CheckInForm from './CheckIn/CheckInForm';
 import Footer from './Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <h1>Welcome to Hope Auto Check-In</h1>
-        {/* Add your app content here */}
-        < CheckInForm />
-      </header>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <header className="App-header">
+              {/* <h1>Welcome to Hope Auto Check-In</h1> */}
+              <CheckInForm />
+            </header>
+          } />
+          <Route path="/admin" element={<AdminLogin />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
